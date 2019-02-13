e7002bb1eb3f9e5d3d0a9b4c6715b36a6c7bfc71
CodeIsMyIkigai <Chester.M.Terry@gmail.com>
2019-02-13 00:19:08 -0800

    Add PG 100 drill

---
+++ DatabaseSQLite3.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+
+conn = sqlite3.connect('test.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons (\
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                col_fname TEXT, \
+                col_lname TEXT, \
+                col_email)")
+    conn.commit()
+conn.close()
+
+
+conn = sqlite3.connect('test.db')
+with conn:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_persons (col_fname,col_lname,col_email) VALUES (?,?,?)",
+                ("Sara", "Blow", "SaraBlow@joeblow.com"))
+    cur.execute("INSERT INTO tbl_persons (col_fname,col_lname,col_email) VALUES (?,?,?)",
+                ("Moe", "Blow", "MoeBlow@joeblow.com"))
+    cur.execute("INSERT INTO tbl_persons (col_fname,col_lname,col_email) VALUES (?,?,?)",
+                ("Doe", "Blow", "DoeBlow@joeblow.com"))
+    cur.execute("INSERT INTO tbl_persons (col_fname,col_lname,col_email) VALUES (?,?,?)",
+                ("Kevin", "Bacon", "kbacon@joeblow.com"))
+
+
+
+
+
+    conn.commit()
+conn.close()
